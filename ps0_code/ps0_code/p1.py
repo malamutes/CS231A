446ad6809679f5e1cde244240d28b119d50f6c09
malamutes <hehexdroflmfao@gmail.com>
2024-09-03 10:20:11 +0800

    First Working Session

--- ps0_code/ps0_code/p1.py
+++ ps0_code/ps0_code/p1.py
@@ -11,6 +11,11 @@ def part_a():
 
     # BEGIN YOUR CODE HERE
 
+    M = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 2, 2]])
+    a = np.array([1,1,0])
+    b = np.array([-1,2,5])
+    c = np.array([0,2,3,2])
+
     # END YOUR CODE HERE
     return M, a, b, c
     
@@ -20,6 +25,8 @@ def part_b(a, b):
     aDotb = None
 
     # BEGIN YOUR CODE HERE
+
+    aDotb = np.dot(np.transpose(a), b)
 
     # END YOUR CODE HERE
     return aDotb
@@ -31,6 +38,8 @@ def part_c(a, b):
     
     # BEGIN YOUR CODE HERE
 
+    aProdb = np.multiply(a, b)
+
     # END YOUR CODE HERE
     return aProdb
 
@@ -39,7 +48,11 @@ def part_d(a, b, M):
     # Find (a^T b)Ma
     result = None
     
+    
     # BEGIN YOUR CODE HERE
+    
+    aDotb = np.dot(np.transpose(a), b)
+    result = np.dot(np.multiply(aDotb, M), a)
 
     # END YOUR CODE HERE
     return result
@@ -52,6 +65,8 @@ def part_e(a, M):
 
     # BEGIN YOUR CODE HERE
 
+    newM = np.multiply(M, a)
+
     # END YOUR CODE HERE
     return newM
 
@@ -62,6 +77,23 @@ def part_f(M):
     sortedM = None
     
     # BEGIN YOUR CODE HERE
+
+    sortedM = np.sort(newM, axis = None)
+
+
+    plt.xlabel =("Index")
+
+    plt.ylabel =("Values")
+
+    plt.title('Plot of sorted values in M')
+
+    x = [0,1,2,3,4,5,6,7,8,9,10,11]
+    y= [0,0,0,0,0,1,2,2,4,5,7,8]
+
+    plt.plot(x, y, 'ro--')
+    plt.legend()
+    plt.axis([0, 11, 0,8])
+    plt.show()
 
     # END YOUR CODE HERE
     
